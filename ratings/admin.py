9ea255b10c7eeb78469fa7ccf5c30c6dc739df0a
Yenly Ma <yencodes@gmail.com>
2017-04-03 18:30:28 -0700

    Create first draft of data model.

--- ratings/admin.py
+++ ratings/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Movie, Ratings
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Movie)
+admin.site.register(Ratings)
--- ratings/models.py
+++ ratings/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class User(models.Model):
+    """User of ratings website."""
+
+    email = models.CharField(max_length=64)
+    password = models.CharField(max_length=64)
+    age = models.IntegerField()
+    zipcode = models.CharField(max_length=15)
+
+    def __str__(self):
+        s = "id=%s email=%s"
+
+        return s % (self.id, self.email)
+
+
+class Movie(models.Model):
+    """Movie on ratings website."""
+
+    title = models.CharField(max_length=100)
+    imdb_id = models.CharField(max_length=10)
+    tmdb_id = models.CharField(max_length=10)
+
+    def __str__(self):
+        s = "id=%s title=%s imdb_id=%s tmdb_id=%s"
+
+        return  s % (self.id, self.title, self.imdb_id, self.tmdb_id)
+
+
+class Ratings(models.Model):
+    """Rating of a movie by a user."""
+
+    movie_id = models.ForeignKey(Movie)
+    user_id = models.ForeignKey(User)
+    score = models.IntegerField()
+
+    def __str__(self):
+        s = "id=%s movie_id=%s user_id=%s score=%s"
+
+        return s % (self.id, self.movie_id, self.user_id, self.score)
